e44d3854b14a2454716d907ad29ff3f3b7108fbe
Blazed <samueltheofie@gmail.com>
2023-11-10 18:29:56 +0700

    Changed main class to use Command interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,30 @@
 #include <iostream>
 #include <string>
+#include "include/command.h"
 
 // Main function that uses REPL loop
 int main(int argc, char const *argv[]) {
-    std::string in;
+    Command command;
 
     while (true) {
         std::cout << "db >  ";
-        getline(std::cin, in);
+        std::cin >> command;
 
-        if (in == ".exit" || in == "q") {
-            exit(EXIT_SUCCESS);
-            return 0;
-        } else {
-            std::cout << "Unrecognized command " << in << std::endl;
+        switch (command.executeStatement()) {
+            case META_COMMAND_SUCCESS: {
+                break;
+            }
+            
+            case META_COMMAND_UNRECOGNIZED: {
+                std::cout << "Unrecognized command " << command.getText() << std::endl;
+                break;
+            }
+
+            case META_COMMAND_QUIT: {
+                exit(EXIT_SUCCESS);
+                return 0;
+                break;
+            }
         }
     }
 
