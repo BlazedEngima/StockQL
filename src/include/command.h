0c4db86ed6eee942b6e26ae4053440ff631247ed
Blazed <samueltheofie@gmail.com>
2023-11-11 15:16:31 +0700

    Added querying functionality
    May be updated to support mutliple querying instruction format

--- src/include/command.h
+++ src/include/command.h
@@ -13,9 +13,21 @@
 
 /* Typedefs */
 typedef enum {META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED, META_COMMAND_QUIT} MetaCommandResult;
-typedef enum {STATEMENT_ADD, STATEMENT_REMOVE, STATEMENT_TRADE, STATEMENT_QUERY, STATEMENT_EXIT, STATEMENT_COMMAND_UNRECOGNIZED, STATEMENT_EMPTY} StatementType;
-typedef std::unordered_map<std::string, StatementType> WordList;
+typedef enum {
+    STATEMENT_ADD, STATEMENT_REMOVE, STATEMENT_TRADE, STATEMENT_QUERY, 
+    STATEMENT_EXIT, STATEMENT_COMMAND_UNRECOGNIZED, STATEMENT_EMPTY    
+} StatementType;
+typedef enum {
+    ASK1P, ASK1Q, ASK2P, ASK2Q, ASK3P, ASK3Q, ASK4P, ASK4Q, ASK5P, ASK5Q,
+    BID1P, BID1Q, BID2P, BID2Q, BID3P, BID3Q, BID4P, BID4Q, BID5P, BID5Q,
+    LAST_TRADED_PRICE, LAST_TRADED_QUANTITY, ALL, TOP_5
+} QueryType;
+typedef std::unordered_map<std::string, StatementType> WordMap;
+typedef std::unordered_map<std::string, Side> SideMap;
+typedef std::unordered_map<std::string, QueryType> QueryMap;
 typedef std::shared_ptr<OrderBook> OrderBookPtr;
+typedef std::vector<std::string> StringVector;
+
 
 /* Command Class */
 class Command {
@@ -25,8 +37,15 @@ class Command {
         std::string symbol; // The name of tradable instruments
         Side side; // The side of the order
         StatementType type; // The type of the command
-        float price, last_traded_price; // The price of the order and the last traded price
-        unsigned int quantity, last_traded_quantity; // The quantity of the order and the last traded quantity
+        float price; // The price of the order
+        unsigned int quantity; // The quantity of the order
+
+        /*
+        *   command_args - a vector to store the arguments of the command
+        *   command arguments could be data fields to be added to the order book
+        *   or a query list of fields to be printed
+        */
+        StringVector command_args;
 
         /*
         *   order_book - A pointer to the order book
@@ -43,6 +62,11 @@ class Command {
         *   parse() - Parses the command
         */
         void parse();
+
+        /*
+        *   handle_query(std::ostream &) - Handles the query command
+        */
+        void handle_query(std::ostream &);
 
     public:
         /*
@@ -66,7 +90,7 @@ class Command {
         *   META_COMMAND_QUIT - The command was quit
         *   (to be implemented further)
         */
-        MetaCommandResult executeStatement();
+        MetaCommandResult executeStatement(std::ostream &);
 
         /*
         *   load() - Loads the database from a log file of commands and executes the commands
@@ -78,14 +102,18 @@ class Command {
         */
         friend std::istream& operator>>(std::istream &, Command &);
 
+        /*
+        *   operator<<() - Overloads the << operator for the Command class
+        */
+        friend std::ostream& operator<<(std::ostream &, const Command &);
+
 };
 
 /*
-*   getFirstWord() - Returns the first word of a string
-*   @param in - The string to get the first word of
-*   @return - The first word of the string
-*   Helper function used to determine the type of command
+*   isNumber() - Checks if a string is a number
+*   @param in - The string to check
+*   @return - True if the string is a number, false otherwise
 */
-std::string getFirstWord(const std::string&);
+bool isNumber(const std::string&);
 
 #endif
